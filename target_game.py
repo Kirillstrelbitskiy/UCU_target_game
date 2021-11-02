b3b787bfa8d409f0e095e698a17812b92f0f8ad8
Kirill <strelOK042021@gmail.com>
2021-11-02 17:55:36 +0200

    Get_words

--- target_game.py
+++ target_game.py
@@ -25,13 +25,38 @@ def get_words(f: str, letters: List[str]) -> List[str]:
     Reads the file f. Checks the words with rules and returns a list of words.
     """
 
+    num_letters = []
+    for _ in range(26):
+        num_letters.append(0)
+    
+    for i in letters:
+        print(ord(i) - ord('a'))
+        num_letters[ord(i) - ord('a')] += 1
+
     words_dict = []
 
     with open(f, "r") as file:
         for word in file:
-            words_dict.append(word.lower())
+            word = word.lower().replace('\n', "")
 
-    return []
+            num_letters_cur = []
+            for i in num_letters:
+                num_letters_cur.append(i)
+
+            flag = True
+
+            for c in word:
+                if num_letters_cur[ord(c) - ord('a')] <= 0:
+                    flag = False
+                    break
+                else:
+                    num_letters_cur[ord(c) - ord('a')] -= 1
+
+            if flag:
+                if word not in words_dict:
+                    words_dict.append(word)
+
+    return words_dict
 
 def get_user_words() -> List[str]:
     """
@@ -64,4 +89,4 @@ def results():
     pass
 
 
-print(get_user_words())
+get_words("en", ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
